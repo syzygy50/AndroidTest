4757967236c240f7c92d0ff1f3a9f4aa609f64fa
Martin <mnperry50@gmail.com>
2017-08-01 19:47:09 -0700

    Second commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
